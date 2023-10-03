01369456571b3f425ae5996cfa1ec91369e840d5
Estebanpalamon <116852153+Estebanpalamon@users.noreply.github.com>
2023-10-02 19:21:59 -0500

    Add files via upload
    Aplicación para el cálculo de un seguro de vehículos.
    
    Para el cobro del seguro se tiene una tarifa base y un cobro extra de acuerdo con las reclamaciones realizadas el año anterior.
    
    La tarifa base es del 5% del valor comercial del vehículo.
    
    De acuerdo con las reclamaciones realizadas el año anterior se le incrementa el valor de la siguiente forma:
    
    1 reclamación: 5% (Sobre el valor base)
    
    2 reclamaciones: 15%
    
    3 reclamaciones: 30%
    
    4 o más reclamaciones: 50%.
    
     
    
    Presente el valor base a pagar, el valor del incremento y valor total.
    
    Todos los valores tienen el iva incluido. El porcentaje del iva es del 19%
    
    Presente el valor total discriminado en el valor antes del iva, el valor del iva y
    
    el total a pagar

---
+++ Controllers/Index.cshtml.cs.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+public class IndexModel : PageModel
+{
+    public class InsuranceCalculatorModel
+    {
+        public decimal VehicleValue { get; set; }
+        public int ClaimCountLastYear { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;  // Establecer la fecha por defecto a la actual
+
+        public string CalculateBaseValue()
+        {
+            decimal baseValue = VehicleValue * 0.05m;
+            return baseValue.ToString("C0"); // Mostrar en formato de moneda sin decimales
+        }
+
+        public string CalculateIncrement()
+        {
+            decimal increment = 0m;
+
+            switch (ClaimCountLastYear)
+            {
+                case 1:
+                    increment = CalculateBaseValueAsDecimal() * 0.05m;
+                    break;
+                case 2:
+                    increment = CalculateBaseValueAsDecimal() * 0.15m;
+                    break;
+                case 3:
+                    increment = CalculateBaseValueAsDecimal() * 0.30m;
+                    break;
+                case 4:
+                default:
+                    increment = CalculateBaseValueAsDecimal() * 0.50m;
+                    break;
+            }
+
+            return increment.ToString("C0"); // Mostrar en formato de moneda sin decimales
+        }
+
+        private decimal CalculateBaseValueAsDecimal()
+        {
+            return VehicleValue * 0.05m;
+        }
+
+        public string CalculateTotalBeforeTax()
+        {
+            decimal totalBeforeTax = CalculateBaseValueAsDecimal() + decimal.Parse(CalculateIncrement(), System.Globalization.NumberStyles.Currency);
+            return totalBeforeTax.ToString("C0"); // Mostrar en formato de moneda sin decimales
+        }
+
+        public string CalculateTotalAfterTax()
+        {
+            decimal totalBeforeTax = decimal.Parse(CalculateTotalBeforeTax(), System.Globalization.NumberStyles.Currency);
+            decimal ivaPercentage = 0.19m;
+
+            decimal totalAfterTax = totalBeforeTax * (1 + ivaPercentage);
+            return totalAfterTax.ToString("C0"); // Mostrar en formato de moneda sin decimales
+        }
+    }
+
+    [BindProperty]
+    public InsuranceCalculatorModel CalculatorModel { get; set; }
+
+    public void OnGet()
+    {
+        CalculatorModel = new InsuranceCalculatorModel();
+    }
+
+    public IActionResult OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        return Page();
+    }
+}
